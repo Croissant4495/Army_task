96de0198d3664fcb43ffad4952abffae9d724171
Croissant4495 <abdelrahmanelshabiny@gmail.com>
2023-08-21 12:42:30 +0300

    Created Soldier.h file and made abstract Soldier class

---
+++ Soldier.h
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class soldiers
+{
+public:
+    string name;
+    double damage;
+    string special_ability;
+    string Rank;
+
+    soldiers (string n,double d,string R)
+    {
+        name=n;
+        damage =d;
+        Rank =R;
+    }
+    ~soldiers()
+    {
+        cout << "THE WAR IS OVER!!";
+    }
+  
+    virtual void defend ()=0;
+    virtual void report ()=0;
+    virtual void kill ()=0;
+    virtual void die ()=0;
+
+
+};
+class offensive : public soldiers
+{
+    void attack ()
+    {
+        cout << "ff";
+    }
+} ;
+class defensive : public soldiers
+{
+
+} ;
+class adaptable : public soldiers
+{
+
+} ;
